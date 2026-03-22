d09aef8ce126a5e1558e0ed84dbc0f1a7c0db109
mcouto0xml <murilo.oliveira@sou.inteli.edu.br>
2026-03-22 08:58:45 -0300

    feat: finish producer tested

--- src/backend/producer/internal/dto/telemetryNewDataRequest.go
+++ src/backend/producer/internal/dto/telemetryNewDataRequest.go
@@ -7,5 +7,5 @@ type TelemetryNewDataRequest struct {
 	Presence 			bool 		`json:"presence"`
 	Vibration  			float64 	`json:"vibration"`
 	Luminosity 			float64 	`json:"luminosity"`
-	TankLevel 			float64 	`json:	"tank_level"`
+	TankLevel 			float64 	`json:"tank_level"`
 }
